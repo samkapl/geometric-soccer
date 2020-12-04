f1796e0661883c32a7c006e6864f4cab0d05b723
samkapl <74516473+samkapl@users.noreply.github.com>
2020-12-04 09:08:26 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,15 +4,9 @@ let balls = [];
 //create a variable to hold your avatar
 let me;
 
-let mySound;
-
-function preload() {
-  soundFormats('wav', 'mp3');
-  mySound = loadSound('sound1.mp3');
-}
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(800, 400);
 
   //make one avatar called me
   me = new Avatar(width/2, 300, 3);
@@ -20,8 +14,11 @@ function setup() {
 }
 
 function draw(){
-	background(220);
-
+	background(115, 239, 250);
+ noStroke();
+  fill("green");
+  textSize(100);
+  text("??'()/'??'()/'???'??/'()()", 0,450,);
   me.drawMe();
   me.moveMe();
 
@@ -52,24 +49,41 @@ class Avatar {
 	drawMe(){  // draw the running person
     		stroke("pink");
         strokeWeight(3);
-    		ellipse(this.x, this.y, 40, 40);
-        ellipse(this.x, this.y +10, 40,40)
-        ellipse(this.x, this.y+30, 40,40 )
+    		fill("orange");
+		    ellipse(this.x,this.y,40,30);
+        noStroke();
+        fill(255);
+        ellipse(this.x + 8, this.y, 10, 15);
+        fill(0);
+        ellipse(this.x + 9, this.y, 7, 9);
+        fill(255);
+        ellipse(this.x + 8, this.y - 2, 2, 2);
+        stroke("pink");
+        fill("orange");
+        triangle(this.x - 20, this.y, this.x - 30, this.y - 15, this.x -30, this.y + 15,this.x - 20, this.y);
+
+
+
+
 	}
 
 	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
     }
 
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
     }
-	}
 
-  die(){
-
+    if (keyIsDown(UP_ARROW)) { // if you hold the down arrow, move down by speed
+        this.y -= this.speed;
+      }
+   if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+          this.y += this.speed;
+}
   }
+
 
 }
 
@@ -80,18 +94,20 @@ class Ball {
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
 		this.x = x;
-		this.y = y;
-  	this.speed = speed;
+    this.y = y;
+    this.speed = speed;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-		stroke(0);
-  	strokeWeight(1);
-		fill("purple");
-		ellipse(this.x,this.y,10,10);
-    ellipse(this.x + 10, this.y, 30, 40)
-    line(this.x, this.y, this.x +10, this.y);
+    	stroke(0);
+      strokeWeight(1);
+    	fill("red");
+		  ellipse(this.x,this.y,50,10);
+      fill(0);
+      triangle(this.x + 25, this.y, this.x + 40, this.y - 12, this.x + 40, this.y + 12, this.x + 25, this.y);
+      ellipse(this.x-10, this.y, 5, 5);
+
 	}
 
 	//update the location of the ball, so it moves across the screen
@@ -100,12 +116,10 @@ class Ball {
 		this.y = this.y+.5;
 	}
 
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
     		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
       			this.speed = -this.speed;
-            mySound.setVolume(0.1);
-            mySound.play();
     		}
   	}
 
