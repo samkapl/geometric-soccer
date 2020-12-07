884c10764aacb887965d9885277bb028bdc58b2f
samkapl <74516473+samkapl@users.noreply.github.com>
2020-12-07 11:09:29 -0800

    isInGoal functionality created

--- sketch.js
+++ sketch.js
@@ -1,67 +1,91 @@
 
+let isInGoal = false;
 
+let goalX = 5
+let goalY = 5
 
 function setup() {
   createCanvas(800, 800)
   background(220);
-  goalpostsR = new Goal(5, 5, "blue", ); // (5,5) is LEAST (x,y) possible
-  goalpostsL= new Goal (740,300, "red", );
+  goalpostsR = new Goal(goalX, goalY, "blue",0 ); // (5,5) is LEAST (x,y) possible
+  goalpostsL= new Goal (goalX + 785, goalY + 295, "red",180 );
   ball1= new Ball (100, 100, "purple");
 }
 
 function draw(){
-angleMode(DEGREES);
-goalpostsR.drawGoalPosts(); // draw goal on the right
-goalpostsL.drawGoalPosts(); // draw goal on the left
-ball1.drawBall();
+  angleMode(DEGREES);
+  goalpostsR.drawGoalPosts(); // draw goal on the right
+  goalpostsL.drawGoalPosts(); // draw goal on the left
+  goalpostsR.checkGoal();
+  goalpostsL.checkGoal();
+  ball1.drawBall();
+
 }
 class Goal {  // create goals
 
-	constructor(x,y, color, rotation){
-		    this.x = x;
-    		this.y = y;
-        this.color = color;
-        //this.rotation = rotation;
-	}
+  constructor(x,y, color, rotation){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.rotation = rotation;
+  }
 
-	drawGoalPosts(){ // instructions to draw goal
+  drawGoalPosts(){ // instructions to draw goal
+    push();
+    translate(this.x,this.y);
+    rotate(this.rotation); // needs to be fixed!!
+    stroke(this.color);
+    fill(220); // change to background color when background is finalized
+    strokeWeight(5);
+    beginShape();
+    vertex(50, 0)
+    vertex(50, 0)
+    vertex(0, 0);
+    vertex(0, 100);
+    vertex(50, 100);
+    vertex(50, 100);
+    endShape();
+    pop();
+  }
 
-        stroke(this.color);
-        fill(220); // change to background color when background is finalized
-        strokeWeight(5);
-    		beginShape();
-        vertex(this.x + 50, this.y)
-        vertex(this.x + 50, this.y)
-        vertex(this.x, this.y);
-        vertex(this.x, this.y +100);
-        vertex(this.x+50, this.y + 100);
-        vertex(this.x+50, this.y + 100);
-        endShape();
-      //  rotate(this.rotation); // needs to be fixed!!
+ checkGoal(){
+    if(this.x >= 50 && this.x <= 10 && this.y >= 45 && this.y <= 10){
+      isInGoal = true;
+      console.log ("in goal");
+    }
+}}
 
-	}
+class Ball{
 
-	}
-  class Ball{
+  constructor(x,y, color){
+    this.x = x;
+    this.y = y;
+    this.color = color;
 
-    constructor(x,y, color){
-  		    this.x = x;
-      		this.y = y;
-          this.color = color;
+  }
+  drawBall(){
+    strokeWeight(1);
+    stroke(0);
+    fill(this.color);
+    ellipse(this.x, this.y, 50, 50);
+    fill(0);
+    ellipse(this.x- 10, this.y, 5, 5);
+    ellipse(this.x + 10, this.y, 5, 5);
+    noFill();
+    strokeWeight(5);
+    ellipse(this.x, this.y + 15, 10, 5);
+  }
 
-  	}
-drawBall(){
-strokeWeight(1);
-stroke(0);
-  fill(this.color);
-  ellipse(this.x, this.y, 50, 50);
-fill(0);
-  ellipse(this.x- 10, this.y, 5, 5);
-  ellipse(this.x + 10, this.y, 5, 5);
-noFill();
-strokeWeight(5);
-  ellipse(this.x, this.y + 15, 10, 5);
-}
-
+  moveBall(){
+    if(isInGoal){
+      fill(random(0,255),random(0,255),random(0,255))
+      // points + 1
+      // changeControls
+      // bounce
+    goalY = goalY + random(5,790);
+    goalX = goalX + random(5,790);
+    isInGoal = false;
+    }
+  }
 
   }
