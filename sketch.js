041d79e28fab1b581055a46908716385f8a94a40
JoaquinG1002 <74562425+JoaquinG1002@users.noreply.github.com>
2020-12-07 10:53:35 -0800

    this is the mixed work

--- sketch.js
+++ sketch.js
@@ -1,15 +1,32 @@
 
 
-function setup() {
-  createCanvas(800, 800)
-  background(220);
-  goalposts = new Goal(5, 5, "blue"); // (5,5) is LEAST (x,y) possible
+var counter = 0
+var timeleft = 180
 
+function convertSeconds(s) {
+  var min = floor (s / 60);
+  return nf(min,2) + ':' + nf(sec,2);
 }
 
-function draw(){
+function setup() {
+  background(220);
+  noCanvas();
 
-goalposts.drawGoalPosts(); // draw goal
+var params = getURLParams();
+console.log(params);
+if (params.minute) {
+  var min = params.minute;
+  timeleft = min * 60;
+}
+  var timer = select('#timer');
+  timer.html(convertSeconds(timeleft - counter);
+
+  function timeIt(){
+    counter++;
+    timer.html(convertSeconds(timeleft - counter);
+  }
+  setInterval(timeIt, 1000);
+
 
 }
 
