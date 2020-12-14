048ca0c3dcb4957f9fec535edff11bfc5c510fd9
JoaquinG1002 <74562425+JoaquinG1002@users.noreply.github.com>
2020-12-14 10:30:15 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 
+var p1score = 0;
+var p2score = 0;
+
 
 var counter = 0
-var timeleft = 180
+var timeleft = 30
 
 function convertSeconds(s) {
   var min = floor (s / 60);
@@ -10,7 +13,10 @@ function convertSeconds(s) {
 }
 
 function setup() {
-  noCanvas();
+  createCanvas(800,800);
+background('aqua')
+
+
 
 var params = getURLParams();
 console.log(params);
@@ -19,13 +25,26 @@ if (params.minute) {
   timeleft = min * 60;
 }
   var timer = select('#timer');
-  timer.html(convertSeconds(timeleft - counter);
+  timer.html(convertSeconds(timeleft - counter));
 
-  function timeIt(){
+  function timeIt() {
     counter++;
-    timer.html(convertSeconds(timeleft - counter);
+    timer.html(convertSeconds(timeleft - counter));
+    if (counter == timeleft){
+      counter = 0;
+    }
   }
   setInterval(timeIt, 1000);
+}
+function draw(){
+  noStroke();
+  fill('black');
+  textSize(40)
+
+  text(p1score,400,300)
+  text(p2score,800,300)
+
+
 
 
 }
